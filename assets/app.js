feb1b9bc6a5f5e40ba339be73752b1ede2aaba81
19Trojan69 <wolfmair.marcus@gmail.com>
2025-11-23 18:56:33 +0100

    Update Supabase anon key in app.js

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const MAPTILER_KEY = "CreAh02QGNcepAT2Zcfm";
 const SUPABASE_URL = "https://mubfgqihjdczrsadrhhz.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11YmZncmloamRjenJzYWRyaGh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3NjI0MDksImV4cCI6MjA0NzMzODQwOX0.KwGieF0kP0xXo7frx1yX9oJXWvLaGybhoVN-d4G8Sho";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11YmZncWloamRjenJzYWRyaGh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyMDczMTAsImV4cCI6MjA3ODc4MzMxMH0.0i2S0o4rOB4I2Np-tPnvMjYfIsB_CZZdZ5w_I83UAk4";
 const TABLE_NAME = "places";
 
 const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
