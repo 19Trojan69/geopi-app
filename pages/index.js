774574e029bb691c65f2cf743310c17dd1e521f6
19Trojan69 <wolfmair.marcus@gmail.com>
2025-11-27 20:07:12 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const MapView = dynamic(() => import('@/components/MapView'), {
+const MapView = dynamic(() => import('../components/MapView'), {
   ssr: false
 });
 
